b3c397f4d8b446acaa436315112afc92925d96ca
mkmutaki <mkmutaki@gmail.com>
2023-02-18 01:04:32 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "kerial-purple": "#431b6d",
+        "back-shapes": "#d9caef",
+      },
+    },
+    fontFamily: {
+      Latos: ["Lato, sans-serif"],
+    },
+    container: {
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+  },
+  plugins: [],
+};
