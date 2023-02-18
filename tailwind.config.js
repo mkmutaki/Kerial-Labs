a9b68f59e4c77b4d083117a7c054f6e9f1b4dfec
mkmutaki <mkmutaki@gmail.com>
2023-02-18 13:10:02 +0300

    Ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  purge: {
+    content: ["./build/*.html"],
+  },
   theme: {
     extend: {
       colors: {
